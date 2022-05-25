2633f293a5ab5f8d938e50b4e13af16041693ec3
Selena Nguyen <missselenah@gmail.com>
2022-05-25 14:39:24 -0700

    quotes page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,61 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Main from './views/Main'
 import Navbar from './components/Navbar'
+import Form from './components/Form';
 import Quotes from './components/Quotes'
-// import Authors from './components/Authors'
-// import Topics from './components/Topics'
+// import Topics from '../components/Topics';
+// import Authors from '../components/Authors';
+import {Routes, Route} from 'react-router-dom';
 
 
+const App = () => {
+  const [allQuotes, setAllQuotes] = useState([]);
+  // const [update, setUpdate] = useState(false);
+
+  useEffect (() => {
+      axios.get("http://localhost:8000/api/quotes")
+      .then(res => {
+          console.log(res.data);
+          setAllQuotes(res.data)
+      })
+      .catch(err => {
+          console.log("XXXX", err);
+      })
+  }, [])
 
 
-function App() {
+  // useEffect (() => {
+  //     axios.get("http://localhost:8000/api/quotes")
+  //     .then(res => {
+  //         console.log(res.data);
+  //         setAllQuotes(res.data)
+  //     })
+  //     .catch(err => {
+  //         console.log("XXXX", err);
+  //     })
+  // }, [update])
+
+  // const removeFromDom = quoteId => {
+  //     setAllQuotes(allQuotes.filter((quote) => quote._id !== quoteId));
+  // }
+
+
   return (
     <div>
-      <Main/>
+      {/* <Main/> */}
       <Navbar/>
-      <Quotes/>
+      <div>
+            <Routes>
+                <Route path='/' element={<Main/>} />
+                <Route path='/quotes' element={<Quotes setAllQuotes={setAllQuotes} allQuotes={allQuotes}/>} />
+                <Route path='/quotes/new' element={<Form setAllQuotes={setAllQuotes} allQuotes={allQuotes}/>} />
+                {/* <Route path='/quotes/:id' element={<View/>}/> */}
+                {/* <Route path='/quotes/:id' element={<View setUpdate = {setUpdate} update = {update}/>} /> */}
+            </Routes>
+        </div>
+
     </div>
   );
 }
