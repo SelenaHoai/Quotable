8aa3c5816174e2498df5e2b91020ff322c082c03
Selena Nguyen <missselenah@gmail.com>
2022-10-26 16:48:11 -0700

    auto populate new authors & topics into dropdown menu

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -18,7 +18,7 @@ const Form = (props) => {
     // function to submit and create a product
     const createQuote = (e) => {
         e.preventDefault();
-
+        
         //create the obj
         const newQuote = {
             author,
@@ -33,7 +33,7 @@ const Form = (props) => {
         axios.post("http://localhost:8000/api/quotes/new", newQuote)
         .then(res => {
             console.log("SUCCESS", res.data)
-            props.setAllQuotes([...props.allQuotes,res.data]);
+            props.setAllDaQuotes([...props.allDaQuotes,res.data]);
             navigate('/quotes', {replace:true})
         })
         .catch(err => {
@@ -59,7 +59,7 @@ const Form = (props) => {
 
     // const handleCancel = (e) => {
     //     e.preventDefault()
-    //     navigate('/pirates')
+    //     navigate('/quotes')
     // }
         
     return (
@@ -71,25 +71,13 @@ const Form = (props) => {
                 </div>
                 <form onSubmit={createQuote}>
                     <div>
-                        <label>Author Name: </label>
-                        <select onChange={(e) =>setAuthor(e.target.value)} value={author}>
-                            <option value="reesewitherspoon">Reese Witherspoon</option>
-                            <option value="elberthubbard">Elbert Hubbard</option>
-                            <option value="mothertheresa">Mother Theresa</option>
-                            <option value="thichnhathanh">Thich Nhat Hanh</option>
-                        </select>
+                        Author Name: <input onChange={(e) =>setAuthor(e.target.value)} value={author} /> <br />
                     </div>
                     <div style={{marginTop:10, marginBottom:10}}>
-                        <label>Topic: </label>
-                        <select onChange={(e) =>setTopic(e.target.value)} value={topic}>
-                            <option value="motivation">Motivation</option>
-                            <option value="travel">Travel</option>
-                            <option value="love">Love</option>
-                        </select>
+                        Topic: <input onChange={(e) =>setTopic(e.target.value)} value={topic} /> <br />
                     </div>
                     <div>
-                        <label>Quote: </label>
-                        <input onChange={(e) =>setQuote(e.target.value)} value={quote}/><br />
+                        Quote: <input onChange={(e) =>setQuote(e.target.value)} value={quote} /><br />
                     </div>
                     <div>
                         <div style={{marginTop:10}}>
@@ -105,6 +93,110 @@ const Form = (props) => {
         </div>
     )
 }
+
+
+// // EXPORT
+// const Form = (props) => {
+//     const [author, setAuthor] = useState("");
+//     const [topic, setTopic] = useState("");
+//     const [quote, setQuote] = useState("");
+
+//     const [isFavorite, setIsFavorite] = useState(true);
+
+//     const [errors, setErrors] = useState([]); 
+//     const navigate = useNavigate();
+
+//     // function to submit and create a product
+//     const createQuote = (e) => {
+//         e.preventDefault();
+
+//         //create the obj
+//         const newQuote = {
+//             author,
+//             topic,
+//             quote,
+//             isFavorite,
+//         }
+//         console.log(newQuote);
+
+//         // go to the server and pass the obj using post
+//         // match the post route with the SERVER ROUTE
+//         axios.post("http://localhost:8000/api/quotes/new", newQuote)
+//         .then(res => {
+//             console.log("SUCCESS", res.data)
+//             props.setAllQuotes([...props.allQuotes,res.data]);
+//             navigate('/quotes', {replace:true})
+//         })
+//         .catch(err => {
+//             const errorResponse = err.response.data.errors; 
+//             const errorArr = []; 
+//             for (const key of Object.keys(errorResponse)) { 
+//                 errorArr.push(errorResponse[key].message)
+//             }
+//             setErrors(errorArr);
+//         })
+//     }
+
+//     function handleCheckBox1 () {
+//         setIsFavorite ((currentCondition) => {
+//             return setIsFavorite(!currentCondition);
+//         })
+//     }
+
+        
+//     // const handleCancel = () => {
+//     //     navigate('/author', {replace:true})
+//     // }
+
+//     // const handleCancel = (e) => {
+//     //     e.preventDefault()
+//     //     navigate('/pirates')
+//     // }
+        
+//     return (
+//         <div className="add-quote-page">
+//             <div className="add-form">
+//                 <div style={{display:"flex", justifyContent:"space-between", margin:10}}>
+//                     <h2>Add Quote</h2>
+//                     <button><Link to={"/"} style={{textDecoration:"none", color:"black", paddingLeft:10, paddingRight:10}}>Home</Link></button>
+//                 </div>
+//                 <form onSubmit={createQuote}>
+//                     <div>
+//                         <label>Author Name: </label>
+//                         <select onChange={(e) =>setAuthor(e.target.value)} value={author}>
+//                             <option value="reesewitherspoon">Reese Witherspoon</option>
+//                             <option value="elberthubbard">Elbert Hubbard</option>
+//                             <option value="mothertheresa">Mother Theresa</option>
+//                             <option value="thichnhathanh">Thich Nhat Hanh</option>
+//                             <option value="Stephen Chbosky">Stephen Chbosky</option>
+//                         </select>
+//                     </div>
+//                     <div style={{marginTop:10, marginBottom:10}}>
+//                         <label>Topic: </label>
+//                         <select onChange={(e) =>setTopic(e.target.value)} value={topic}>
+//                             <option value="motivation">Motivation</option>
+//                             <option value="travel">Travel</option>
+//                             <option value="love">Love</option>
+//                         </select>
+//                     </div>
+//                     <div>
+//                         <label>Quote: </label>
+//                         <input onChange={(e) =>setQuote(e.target.value)} value={quote}/><br />
+//                     </div>
+//                     <div>
+//                         <div style={{marginTop:10}}>
+//                             <input onClick={handleCheckBox1} onChange={(e) => setIsFavorite(e.target.value)} value={isFavorite} checked={isFavorite ? 'checked' : ''} type="checkbox" /> Favorite
+//                         </div>
+//                         {errors.map((err, index) => <p key={index} style={{color:"red"}}>{err}</p>)}
+//                         <div style={{display:"flex", justifyContent:"center"}}>
+//                             <button style={{margin:10}}>Add Quote</button> 
+//                         </div>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default Form
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
-import React, {useState} from 'react';
-import axios from 'axios';
-import {useNavigate, useParams} from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+// import React, {useState} from 'react';
+// import axios from 'axios';
+// import {useNavigate, useParams} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = (props) => {
     // const [author, setAuthor] = useState("");
     // const [topic, setTopic] = useState("");
-    const {topic} = useParams();
-    const {author} = useParams();
+    // const {topic} = useParams();
+    // const {author} = useParams();
     const navigate = useNavigate();
     // const [flip, setFlip] = useState("");
 
@@ -38,27 +41,35 @@ const hideTopicDropdown = () => {
   return (
     <div className="nav-bar">
       <div>
-        <a href="/"><img src="./images/Quotable-logoblk.png" width="230" height="130"></img></a>
+        <a href="/"><img src="./images/Quotable-logoblk.png" alt="QuotableLogo" width="230" height="130"></img></a>
       </div>
       <div className="nav-bar">
         <button className="allQuotes" onClick={goToQuotes}>Quotes</button>
         <div className="dropdown" onMouseOver={showAuthorDropdown} onMouseOut={hideAuthorDropdown}>
           <button className="dropbtn">Authors <i className="fa fa-caret-down"></i></button>
-          <div id="dropdown_author_content">
-            <a href={`/quotes/authors/reesewitherspoon`} data-id="reesewitherspoon">Reese Witherspoon</a>
-            <a href={`/quotes/authors/elberthubbard`} data-id="elberthubbard">Elbert Hubbard</a>
-            <a href={`/quotes/authors/mothertheresa`} data-id="mothertheresa">Mother Theresa</a>
-            <a href={`/quotes/authors/thichnhathanh`} data-id="thichnhathanh">Thich Nhat Hanh</a>
-          </div>
+          <div id="dropdown_author_content">{
+            props.allDaQuotes?.map((theQuote) => {
+              return (
+                <div key={theQuote._id}>
+                  <Link to={`/quotes/authors/${theQuote._id}`}>{theQuote.author}</Link>
+                </div>
+              )
+            })
+          }</div>
         </div>
+        
         <div>
         <div className="dropdown" onMouseOver={showTopicDropdown} onMouseOut={hideTopicDropdown}>
           <button className="dropbtn">Topics <i className="fa fa-caret-down"></i></button>
-          <div id="dropdown_topic_content">
-            <a href={`/quotes/topics/love`} data-id="love">Love</a>
-            <a href={`/quotes/topics/motivation`} data-id="motivation">Motivation</a>
-            <a href={`/quotes/topics/travel`} data-id="travel">Travel</a>
-          </div>
+          <div id="dropdown_topic_content">{
+            props.allDaQuotes?.map((theQuote) => {
+              return (
+                <div key={theQuote.id}>
+                  <Link to={`/quotes/topics/${theQuote.id}`}>{theQuote.topic}</Link>
+                </div>
+              )
+            })
+          }</div>
         </div>
         </div>
       </div>
@@ -69,5 +80,73 @@ const hideTopicDropdown = () => {
     </div>
   );
 }
+
+// ******************************************
+
+// const Navbar = () => {
+//   // const [author, setAuthor] = useState("");
+//   // const [topic, setTopic] = useState("");
+//   const {topic} = useParams();
+//   const {author} = useParams();
+//   const navigate = useNavigate();
+//   // const [flip, setFlip] = useState("");
+
+
+// const goToQuotes = () => {
+//     navigate("/quotes");
+// }
+
+// const showAuthorDropdown = () => {
+//     document.getElementById("dropdown_author_content").style.display="inherit";
+// }
+
+// const hideAuthorDropdown = () => {
+//     document.getElementById("dropdown_author_content").style.display="none";
+// }
+
+// const showTopicDropdown = () => {
+//   document.getElementById("dropdown_topic_content").style.display="inherit";
+// }
+
+// const hideTopicDropdown = () => {
+//   document.getElementById("dropdown_topic_content").style.display="none";
+// }
+
+
+// return (
+//   <div className="nav-bar">
+//     <div>
+//       <a href="/"><img src="./images/Quotable-logoblk.png" width="230" height="130"></img></a>
+//     </div>
+//     <div className="nav-bar">
+//       <button className="allQuotes" onClick={goToQuotes}>Quotes</button>
+//       <div className="dropdown" onMouseOver={showAuthorDropdown} onMouseOut={hideAuthorDropdown}>
+//         <button className="dropbtn">Authors <i className="fa fa-caret-down"></i></button>
+//         <div id="dropdown_author_content">
+//           <a href={`/quotes/authors/reesewitherspoon`} data-id="reesewitherspoon">Reese Witherspoon</a>
+//           <a href={`/quotes/authors/elberthubbard`} data-id="elberthubbard">Elbert Hubbard</a>
+//           <a href={`/quotes/authors/mothertheresa`} data-id="mothertheresa">Mother Theresa</a>
+//           <a href={`/quotes/authors/thichnhathanh`} data-id="thichnhathanh">Thich Nhat Hanh</a>
+//           <a href={`/quotes/authors/Stephen Chbosky`} data-id="Stephen Chbosky">Stephen Chbosky</a>
+//         </div>
+//       </div>
+//       <div>
+//       <div className="dropdown" onMouseOver={showTopicDropdown} onMouseOut={hideTopicDropdown}>
+//         <button className="dropbtn">Topics <i className="fa fa-caret-down"></i></button>
+//         <div id="dropdown_topic_content">
+//           <a href={`/quotes/topics/love`} data-id="love">Love</a>
+//           <a href={`/quotes/topics/motivation`} data-id="motivation">Motivation</a>
+//           <a href={`/quotes/topics/travel`} data-id="travel">Travel</a>
+//         </div>
+//       </div>
+//       </div>
+//     </div>
+//     {/* <div className="nav-bar">
+//       <button onClick="#" style={{marginRight:10, color:"blue", backgroundColor:"white"}}>Register</button>
+//       <button onClick="#" style={{marginRight:10, color:"green"}}>Login</button>
+//     </div> */}
+//   </div>
+// );
+// }
 
 export default Navbar;
